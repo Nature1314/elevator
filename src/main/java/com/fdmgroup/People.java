12fb8ae792ba24b0ccac1ea245da0b8841e9c730
Nature1314 <Nature1314@users.noreply.github.com>
2018-10-31 23:48:21 +0800

    The people class provides the information of the floor number that people there and the floor number that people wants to go

---
+++ src/main/java/com/fdmgroup/People.java
@@ -0,0 +1,30 @@
+package com.fdmgroup;
+
+public class People {
+	
+	private int floorEnterNum;
+	private int floorExitNum;
+	 
+	public People(int floorEnterNum, int floorExitNum) {
+		this.floorEnterNum = floorEnterNum;
+		this.floorExitNum = floorExitNum;
+	}
+
+	public int getFloorEnterNum() {
+		return floorEnterNum;
+	}
+
+	public void setFloorEnterNum(int floorEnterNum) {
+		this.floorEnterNum = floorEnterNum;
+	}
+
+	public int getFloorExitNum() {
+		return floorExitNum;
+	}
+
+	public void setFloorExitNum(int floorExitNum) {
+		this.floorExitNum = floorExitNum;
+	}
+	
+ 
+}
