1b1564e6a7539225b6792f05ce4ed6351c968d13
Nature1314 <Nature1314@users.noreply.github.com>
2018-11-02 13:30:04 +0800

    add new tests for the class

--- src/test/java/com/fdmgroup/ControllerTest.java
+++ src/test/java/com/fdmgroup/ControllerTest.java
@@ -1,41 +1,58 @@
 package com.fdmgroup;
 
 import org.junit.Test;
-
+import org.mockito.Mockito;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import java.util.ArrayList;
 
 public class ControllerTest {
-
 	@Test
-		public void Test_IfAvaliable_Returnavaliableelevator() {
-			System.out.println("Test get avalible elevator list");
-			Controller controller = new Controller(5, 1, 5);
-			ArrayList<Elevator> myTastList = controller.getAvalibleElevatorList(0,5);
-			assertTrue(myTastList.size()==1);
-		}
-	
-	@Test
-	public void Test_chooseElevator_return_Elevator() {
-		System.out.println("Test chooseElevator");		
-		Elevator mockElevator = mock(Elevator.class);		
-		Controller controller = new Controller(5,1,1);
-		People p1 = new People(1,5);
-		controller.chooseElevator(p1);
-		verify(mockElevator).addTasks(1, 5);
-	
+	public void test_addTasksAndDistributeThem() {
+		System.out.println("Test add tasks and distribute task");
+		Controller controller = Mockito.spy(new Controller(2,2,2));
+		People person = new People(2,3);
+		controller.addTasksAndDistributeThem(person);
+		verify(controller).chooseElevator(person);
 	}
 	
 	@Test
-	public void test_addTasksAndDistributeThem_verifyIfTheWaitingListIsNotEmpty() {
-		System.out.println("Test addTasksAndDistributeThem");
-		Controller controller = new Controller(15,1,4);
-		People p1 = new People(0,4);
-		People p2 = new People(3,2);
-		controller.addTasksAndDistributeThem(p1,p2);
-		assertTrue(true);
+	public void test_chooseElevator() {
+		System.out.println("Test choose elevator");
+		Controller controller = Mockito.spy(new Controller(2,2,2));
+		People person = new People(2,3);
+		controller.chooseElevator(person);
+		verify(controller).getAvalibleElevatorList(2, 3);
 	}
-
+	
+	@Test
+	public void test_getAvalibleElevatorList() {
+		System.out.println("Test get avalible elevator list");
+		Controller controller = new Controller(5, 1, 5);
+		ArrayList<Elevator> myTastList = controller.getAvalibleElevatorList(0,5);
+		assertTrue(myTastList.size()==1);
+	}
+	
+	@Test
+	public void test_calculateTimeAndselectMin() {
+		System.out.println("Test calculate time and select min");
+		Controller controller = new Controller(5, 1, 5);
+		People person = new People(2,3);
+		ArrayList<Elevator> myTastList = controller.getAvalibleElevatorList(2,3);
+		Elevator expResult = myTastList.get(0);
+	    Elevator result = controller.calculateTimeAndselectMin(myTastList, person);
+	    assertEquals(expResult, result);
+	}
+	
+	@Test
+	public void test_taskBetweenPeopleAndLift() {
+		System.out.println("Test task between people and lift");
+		Controller controller = new Controller(5, 1, 5);
+		//People person = new People(2,3);
+		ArrayList<Elevator> myTastList = controller.getAvalibleElevatorList(2,3);
+		int expResult = 0;
+	    int result = controller.taskBetweenPeopleAndLift(myTastList.get(0), 2);
+	    assertEquals(expResult, result);
+	}
 
 }
